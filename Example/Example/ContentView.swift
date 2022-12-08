9d41a4040352fc50b4d79bcfa2bef87914b35dc7
Kristof Kalai <kristof.kalai@supercharge.io>
2022-12-08 22:36:23 +0100

    feat: add example app

---
+++ Example/Example/ContentView.swift
@@ -0,0 +1,32 @@
+//
+//  ContentView.swift
+//  Example
+//
+//  Created by Kristof Kalai on 2022. 12. 08..
+//
+
+import SwiftUI
+import YouTubePlayPauseView
+
+struct ContentView: View {
+    @State private var playing = false
+
+    var body: some View {
+        YouTubePlayPauseView(playing: $playing, continuousUpdate: true, color: .red)
+            .frame(width: 100, height: 100)
+//            .padding(32)
+//            .background(.black.opacity(0.5))
+//            .mask {
+//                Circle()
+//            }
+            .onChange(of: playing) {
+                print("Playing: \($0)")
+            }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
